bce8f19ecd2920cbe31af85297549cb6fbe0ec61
tarunsunny3 <tarunsunny3@gmail.com>
2024-03-01 16:07:19 -0700

    Update Backend Heroku URL in frontend

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://34.94.19.218/auth/";
+const API_URL = process.env.REACT_APP_API_URL + "/auth/";
 
 class AuthService {
   async login(email: string, password: string) {
